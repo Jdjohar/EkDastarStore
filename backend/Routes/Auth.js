b4164a99d6f5cad09ad18f75857ded16b78daafd
Jdjohar <Jdeep514@gmail.com>
2024-06-22 19:01:43 +0530

    Update in Store 2

--- backend/Routes/Auth.js
+++ backend/Routes/Auth.js
@@ -295,7 +295,7 @@ router.post('/api/products', upload.single('img'), async (req, res) => {
           name,
           description,
           CategoryName,
-          img:`http://localhost:5000/${img}`,
+          img:`https://store-ywot.onrender.com/${img}`,
           options: JSON.parse(options)
         });
         
@@ -328,7 +328,7 @@ router.post('/api/category', upload.single('catimg'), async (req, res) => {
       const newCategory = new Category({
          
           CategoryName,
-          img:`http://localhost:5000/${img}`,
+          img:`https://store-ywot.onrender.com/${img}`,
         });
         
       
